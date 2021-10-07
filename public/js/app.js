86abf42ab3b93767fd12d902b721555d8e1b2900
Johan <klausenjohan23@gmail.com>
2021-10-07 14:56:28 +0200

    added a img to the hmtl file

--- public/js/app.js
+++ public/js/app.js
@@ -7,12 +7,17 @@ xhttp.onload = () => {
     const data = xhttp.response;
     console.log(data)
 
+    var imgUrl = data.articleImage;
+    
     let headlineH1 = document.createElement('h1');
     let introP = document.createElement('p');
+    let articleIMG = document.createElement('img');
 
     headlineH1.textContent = data.headline;
     introP.textContent = data.intro;
+    articleIMG.setAttribute('src', imgUrl);
 
     document.getElementById('Exercise1').appendChild(headlineH1);
     document.getElementById('Exercise1').appendChild(introP);
+    document.getElementById('Exercise1').appendChild(articleIMG)
 };
