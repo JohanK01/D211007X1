928839c2b35c3d2d6d144b2796cce5b3f9c52281
Johan <klausenjohan23@gmail.com>
2021-10-07 20:13:32 +0200

    added edit.html but uses the same controller

--- controller/appController.js
+++ controller/appController.js
@@ -2,7 +2,6 @@ exports.app = (req, res) => {
     res.sendfile ('public/data/app.html');
 };
 
-// exports.app = (req, res) => {
-//     const data = require('../public/data/exercise1.json')
-//     res.json(data);
-// }
+exports.edit = (req, res) => {
+    res.sendfile ('public/data/edit.html');
+};
--- routes/app.js
+++ routes/app.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const app_Controller = require('../controller/appController');
 
 router.get('/', app_Controller.app);
+router.get('/edit', app_Controller.edit);
 
 
 module.exports = router;
