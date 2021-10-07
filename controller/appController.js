32bf5756211501e1d3218700f952ec43a3f97908
Johan <klausenjohan23@gmail.com>
2021-10-07 10:56:37 +0200

    managed to read in the json file and write it out to the html

--- controller/appController.js
+++ controller/appController.js
@@ -1,3 +1,8 @@
 exports.app = (req, res) => {
     res.sendfile ('public/data/app.html');
-}
+};
+
+// exports.app = (req, res) => {
+//     const data = require('../public/data/exercise1.json')
+//     res.json(data);
+// }
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
-console.log('Im alive as well');
+const element = document.getElementById("Exercise1");
+
+const txt = '{"headline" : "Welcome to Exercise1", "intro" : "This is a short introduction"}'
+const obj = JSON.parse(txt);
+element.innerHTML = obj.headline + " " + obj.intro
+
+
+
+// let xhttp = XMLHttpRequest();
+// document.getElementById("Exercise1").innerHTML = xhttp.responseText;
